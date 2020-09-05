ce3d1b684d9cbb84dd9aa85370d115fe92ac9420
RaghadSB <61096506+RaghadSB@users.noreply.github.com>
2020-09-05 06:59:45 +0300

    add api.py file

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -17,16 +17,29 @@ CORS(app)
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
 # db_drop_and_create_all()
+# ROUTES
+# ------------------------------------------------------------------------------
 
-## ROUTES
 '''
 @TODO implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    returns status code 200 and json {"success": True, "drinks": drinks}
+     where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks', methods=['GET'])
+def drinks():
+    try:
+        thedrink = Drink.query.all()
+        drinkforma = [d.long() for d in thedrink]
+        return jsonify({"success": True, "drinks": drinkforma})
+    except:
+        abort(422)
+# ------------------------------------------------------------------------------
 
 
 '''
@@ -34,9 +47,22 @@ CORS(app)
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    returns status code 200 and json {"success": True, "drinks": drinks}
+     where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks-detail')
+@requires_auth('get:drinks-detail')
+def get_drinks(payload):
+    try:
+        thedrink = Drink.query.all()
+        drinkforma = [d.long() for d in thedrink]
+        return jsonify({"success": True, "drinks": drinkforma})
+    except:
+        abort(422)
+# ------------------------------------------------------------------------------
 
 
 '''
@@ -45,9 +71,26 @@ CORS(app)
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+    returns status code 200 and json {"success": True, "drinks": drink}
+    where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def add_drinks(payload):
+
+    title = request.get_json()['title']
+    recipe = request.get_json()['recipe']
+    try:
+        drink = Drink(title=title,   recipe=json.dumps(recipe))
+        drink.insert()
+        return jsonify({'success': True, 'drinks': drink.long()})
+    except:
+        abort(422)
+
+# ..........................................................................................
 
 
 '''
@@ -58,11 +101,43 @@ CORS(app)
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+        returns status code 200 and json {"success": True, "drinks": drink}
+        where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
 
 
+@app.route('/drinks/<int:drink_id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def patch_drinks(payload, drink_id):
+    all_drinks = Drink.query.all()
+    #  one or none did not work for me so i came up with this way
+    #  flag set to false unless i found matxh id, and is so then set the flasg
+    # to true otherwise abort 404 error
+    flag = False
+    for da in all_drinks:
+        if drink_id == da.id:
+            flag = True
+    if not flag:
+        abort(404)
+    if flag:
+        try:
+            thedata = request.get_json()
+            thedrink = Drink.query.get(drink_id)
+            if 'title' in thedata:
+                thedrink.title = thedata.get('title', thedrink.title)
+            if 'recipe' in thedata:
+                thedrink.recipe = json.dumps(
+                    thedata.get('recipe', thedrink.recipe))
+            thedrink.update()
+            return jsonify({'success': True, 'drinks': [thedrink.long()]})
+
+        except:
+
+            abort(422)
+
+
+# ------------------------------------------------------------------------------
 '''
 @TODO implement endpoint
     DELETE /drinks/<id>
@@ -70,41 +145,108 @@ CORS(app)
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+    returns status code 200 and json {"success": True, "delete": id}
+     where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
 
 
-## Error Handling
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drinks(payload, drink_id):
+    all_drinks = Drink.query.all()
+    flag = False
+    for da in all_drinks:
+        if drink_id == da.id:
+            flag = True
+    if not flag:
+        abort(404)
+    if flag:
+        try:
+            thedrink = Drink.query.get(drink_id)
+            if thedrink:
+                thedrink.delete()
+            return jsonify({"success": True, "delete": drink_id})
+        except:
+            abort(422)
+# ------------------------------------------------------------------------------
+
+
+# Error Handling
 '''
 Example error handling for unprocessable entity
-'''
-@app.errorhandler(422)
-def unprocessable(error):
-    return jsonify({
-                    "success": False, 
-                    "error": 422,
-                    "message": "unprocessable"
-                    }), 422
-
-'''
 @TODO implement error handlers using the @app.errorhandler(error) decorator
     each error handler should return (with approprate messages):
              jsonify({
-                    "success": False, 
+                    "success": False,
                     "error": 404,
                     "message": "resource not found"
                     }), 404
+'''
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
+
+
+@app.errorhandler(403)
+def Forbidden(error):
+    return jsonify({
+        "success": False,
+        "error": 403,
+        "message": "Access is forbidden to the requested page."
+    }), 403
+
+
+@app.errorhandler(500)
+def InternalError(error):
+    return jsonify({
+        "success": False,
+        "error": 500,
+        "message": "Internal Server Error."
+    }), 500
+
+
+@app.errorhandler(400)
+def BadRequest(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "Bad Request."
+    }), 400
+
 
 '''
 
+'''
 '''
 @TODO implement error handler for 404
-    error handler should conform to general task above 
+    error handler should conform to general task above
 '''
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "The server can not find the requested page."
+    }), 404
 
 
 '''
 @TODO implement error handler for AuthError
-    error handler should conform to general task above 
+    error handler should conform to general task above
 '''
+
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
